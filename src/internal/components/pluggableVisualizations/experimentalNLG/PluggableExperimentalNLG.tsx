41a805460b7e533103cdb151a11a83c4b1437b96
Vlastimil Kotas <vlastimil.kotas@gooddata.com>
2019-09-17 15:07:21 +0200

    RELATED: ONE-4018 Optimize `PluggableExperimentalNLG` properties

--- src/internal/components/pluggableVisualizations/experimentalNLG/PluggableExperimentalNLG.tsx
+++ src/internal/components/pluggableVisualizations/experimentalNLG/PluggableExperimentalNLG.tsx
@@ -1,78 +1,73 @@
-import * as React from 'react';
-import { InjectedIntl } from 'react-intl';
-import { render, unmountComponentAtNode } from 'react-dom';
-import cloneDeep = require('lodash/cloneDeep');
-import { VisualizationObject, AFM } from '@gooddata/typings';
+import * as React from "react";
+import { InjectedIntl } from "react-intl";
+import { render, unmountComponentAtNode } from "react-dom";
+import cloneDeep = require("lodash/cloneDeep");
+import { VisualizationObject, AFM } from "@gooddata/typings";
 import * as BucketNames from "../../../../constants/bucketNames";
 
-import get = require('lodash/get');
-import set = require('lodash/set');
+import get = require("lodash/get");
+import set = require("lodash/set");
 import {
-   IReferencePoint,
-   IExtendedReferencePoint,
-   IVisCallbacks,
-   IVisConstruct,
-   IVisProps,
-   ILocale,
-   IUiConfig,
-   IBucketItem,
-   IVisualizationProperties
-} from '../../../interfaces/Visualization';
+    IReferencePoint,
+    IExtendedReferencePoint,
+    IVisCallbacks,
+    IVisConstruct,
+    IVisProps,
+    ILocale,
+    IUiConfig,
+    IBucketItem,
+    IVisualizationProperties,
+} from "../../../interfaces/Visualization";
 
-import {
-    METRIC,
-    FACT,
-    ATTRIBUTE,
-    DATE,
-    BUCKETS
-} from '../../../constants/bucket';
+import { METRIC, FACT, ATTRIBUTE, DATE, BUCKETS } from "../../../constants/bucket";
 
 import {
     getMeasureItems,
     getDateItems,
     getAllAttributeItemsWithPreference,
-    sanitizeUnusedFilters
-} from '../../../utils/bucketHelper';
+    sanitizeUnusedFilters,
+} from "../../../utils/bucketHelper";
 
 import { createInternalIntl } from "../../../utils/internalIntlProvider";
 import { DEFAULT_LOCALE } from "../../../../constants/localization";
 
-import { ExperimentalNLG } from './ExperimentalNLG';
+import { ExperimentalNLG } from "./ExperimentalNLG";
 
-import {
-    removeInvalidSort
-} from '../../../utils/sort';
+import { removeInvalidSort } from "../../../utils/sort";
 import { AbstractPluggableVisualization } from "../AbstractPluggableVisualization";
 
 export class PluggableExperimentalNLG extends AbstractPluggableVisualization {
-   private callbacks: IVisCallbacks;
-   protected intl: InjectedIntl;
-   private element: string;
-   protected environment: string;
-   private locale: ILocale;
+    private callbacks: IVisCallbacks;
+    protected intl: InjectedIntl;
+    private element: string;
+    protected environment: string;
+    private locale: ILocale;
 
-   constructor(props: IVisConstruct) {
-       super();
-       this.element = props.element;
-       this.environment = props.environment;
-       this.callbacks = props.callbacks;
-       this.locale = props.locale ? props.locale : DEFAULT_LOCALE;
-       this.intl = createInternalIntl(this.locale);
-   }
-   public unmount() {
-       unmountComponentAtNode(document.querySelector(this.element) );
-   }
+    constructor(props: IVisConstruct) {
+        super();
+        this.element = props.element;
+        this.environment = props.environment;
+        this.callbacks = props.callbacks;
+        this.locale = props.locale ? props.locale : DEFAULT_LOCALE;
+        this.intl = createInternalIntl(this.locale);
+    }
+    public unmount() {
+        unmountComponentAtNode(document.querySelector(this.element));
+    }
 
-   public update(options: IVisProps, visualizationProperties: IVisualizationProperties
-   ,             mdObject: VisualizationObject.IVisualizationObjectContent) {
-         const resultSpecWithDimensions: AFM.IResultSpec = {
+    public update(
+        options: IVisProps,
+        visualizationProperties: IVisualizationProperties,
+        mdObject: VisualizationObject.IVisualizationObjectContent,
+    ) {
+        const resultSpecWithDimensions: AFM.IResultSpec = {
             ...options.resultSpec,
-            dimensions: this.getDimensions(mdObject)
+            dimensions: this.getDimensions(mdObject),
         };
 
-         const { onError, onLoadingChanged, pushData } = this.callbacks;
+        const { onError, onLoadingChanged, pushData } = this.callbacks;
 
-         render(
+        render(
             <ExperimentalNLG
                 mdObject={mdObject}
                 onError={onError}
@@ -82,11 +77,11 @@ export class PluggableExperimentalNLG extends AbstractPluggableVisualization {
                 resultSpec={resultSpecWithDimensions}
                 visualizationProperties={visualizationProperties}
             />,
-            document.querySelector(this.element)
+            document.querySelector(this.element),
         );
     }
 
-   public getExtendedReferencePoint(referencePoint: IReferencePoint): Promise<IExtendedReferencePoint> {
+    public getExtendedReferencePoint(referencePoint: IReferencePoint): Promise<IExtendedReferencePoint> {
         const clonedReferencePoint = cloneDeep(referencePoint);
 
         const buckets = get(clonedReferencePoint, BUCKETS, []);
@@ -98,93 +93,91 @@ export class PluggableExperimentalNLG extends AbstractPluggableVisualization {
         if (dateItems.length) {
             attributes = [...dateItems];
         } else {
-            attributes = getAllAttributeItemsWithPreference(buckets, [BucketNames.TREND,
-                                                                      BucketNames.VIEW,
-                                                                      BucketNames.SEGMENT,
-                                                                      BucketNames.STACK]
-            );
+            attributes = getAllAttributeItemsWithPreference(buckets, [
+                BucketNames.TREND,
+                BucketNames.VIEW,
+                BucketNames.SEGMENT,
+                BucketNames.STACK,
+            ]);
         }
 
-        set(clonedReferencePoint, BUCKETS, [{
-            localIdentifier: 'measures',
-            items: measures
-        }, {
-            localIdentifier: 'attributes',
-            items: attributes
-        }]);
+        set(clonedReferencePoint, BUCKETS, [
+            {
+                localIdentifier: "measures",
+                items: measures,
+            },
+            {
+                localIdentifier: "attributes",
+                items: attributes,
+            },
+        ]);
 
         const extendedRefrencePoint: IExtendedReferencePoint = {
             ...clonedReferencePoint,
-            uiConfig: this.getUiConfig()
+            uiConfig: this.getUiConfig(),
         };
         removeInvalidSort(extendedRefrencePoint);
 
-        return Promise.resolve(
-            sanitizeUnusedFilters(extendedRefrencePoint, clonedReferencePoint)
-        );
-
+        return Promise.resolve(sanitizeUnusedFilters(extendedRefrencePoint, clonedReferencePoint));
     }
 
-   private getUiConfig(): IUiConfig {
-       return {
-           buckets: {
-               measures: {
-                   accepts: [METRIC, FACT, ATTRIBUTE],
-                   title: 'Measures',
-                   allowsDuplicateItems: true,
-                   enabled: true,
-                   allowsReordering: true,
-                   allowsSwapping: true,
-                   itemsLimit: 20,
-                   isShowInPercentEnabled: false,
-                   isShowInPercentVisible: false
-              },
-               attributes: {
-                   accepts: [ATTRIBUTE, DATE],
-                   title: 'Attributes',
-                   itemsLimit: 20,
-                   allowsSwapping: false,
-                   allowsReordering: true,
-                   enabled: true,
-                   isShowInPercentEnabled: false,
-                   isShowInPercentVisible: false
-              },
-               filters: {
-                   accepts: [ATTRIBUTE, DATE],
-                   itemsLimit: 20,
-                   allowsReordering: false,
-                   enabled: true,
-                   isShowInPercentEnabled: false
-         }
-           }
-       }; }
-
-   private getDimensions(mdObject: VisualizationObject.IVisualizationObjectContent)
-   : AFM.IDimension[] {
-
-       if (mdObject.buckets.length > 1) {
-          const items = mdObject.buckets[1].items;
-          const attributes = items.map((item: any) => item.visualizationAttribute.localIdentifier);
-
-          return [
-               {
-                   itemIdentifiers: ['measureGroup']
-               },
-               {
-                   itemIdentifiers: attributes
-               }
-           ];
-       } else {
-       return [
-               {
-                   itemIdentifiers: ['measureGroup']
-               },
-               {
-                   itemIdentifiers: []
-               }
-           ];
-       }
-
+    private getUiConfig(): IUiConfig {
+        return {
+            buckets: {
+                measures: {
+                    accepts: [METRIC, FACT, ATTRIBUTE],
+                    title: "Measures",
+                    allowsDuplicateItems: true,
+                    enabled: true,
+                    allowsReordering: false,
+                    allowsSwapping: false,
+                    itemsLimit: 1,
+                    isShowInPercentEnabled: false,
+                    isShowInPercentVisible: false,
+                },
+                attributes: {
+                    accepts: [ATTRIBUTE, DATE],
+                    title: "Attributes",
+                    itemsLimit: 1,
+                    allowsSwapping: false,
+                    allowsReordering: false,
+                    enabled: true,
+                    isShowInPercentEnabled: false,
+                    isShowInPercentVisible: false,
+                },
+                filters: {
+                    accepts: [ATTRIBUTE, DATE],
+                    itemsLimit: 20,
+                    allowsReordering: false,
+                    enabled: true,
+                    isShowInPercentEnabled: false,
+                },
+            },
+        };
     }
 
+    private getDimensions(mdObject: VisualizationObject.IVisualizationObjectContent): AFM.IDimension[] {
+        if (mdObject.buckets.length > 1) {
+            const items = mdObject.buckets[1].items;
+            const attributes = items.map((item: any) => item.visualizationAttribute.localIdentifier);
+
+            return [
+                {
+                    itemIdentifiers: ["measureGroup"],
+                },
+                {
+                    itemIdentifiers: attributes,
+                },
+            ];
+        } else {
+            return [
+                {
+                    itemIdentifiers: ["measureGroup"],
+                },
+                {
+                    itemIdentifiers: [],
+                },
+            ];
+        }
+    }
 }
